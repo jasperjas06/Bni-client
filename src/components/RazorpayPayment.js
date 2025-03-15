57cf6bb7feb26b72e1e77c72a4b60f50262424c7
Jasper P <91078061+jasperjas06@users.noreply.github.com>
2025-03-15 12:11:38 +0530

    Update RazorpayPayment.js
    date changed

--- src/components/RazorpayPayment.js
+++ src/components/RazorpayPayment.js
@@ -19,27 +19,27 @@ const RazorpayPayment = () => {
 
   // Payment Data Based on Date Ranges
   const memberPaymentData = [
-    { start: "2025-01-01", end: "2025-03-15", amount: 354 , bannerAmount:300},  
-    { start: "2025-03-16", end: "2025-03-31", amount: 472 ,bannerAmount:400},  
+    { start: "2025-01-01", end: "2025-03-19", amount: 354 , bannerAmount:300},  
+    { start: "2025-03-20", end: "2025-03-31", amount: 472 ,bannerAmount:400},  
     { start: "2025-04-01", end: "2025-04-07", amount: 590 ,bannerAmount:500},  
     { start: "2025-04-08", end: "2025-12-31", amount: 750 ,bannerAmount:750},
   ];
 
   const VisitorPaymentData = [
-    { start: "2025-01-01", end: "2025-03-15", amount: 472 ,bannerAmount:400},  
-    { start: "2025-03-16", end: "2025-03-31", amount: 590 ,bannerAmount:500},  
+    { start: "2025-01-01", end: "2025-03-19", amount: 472 ,bannerAmount:400},  
+    { start: "2025-03-20", end: "2025-03-31", amount: 590 ,bannerAmount:500},  
     { start: "2025-04-01", end: "2025-04-07", amount: 767 ,bannerAmount:700},  
     { start: "2025-04-08", end: "2025-12-31", amount: 900 ,bannerAmount:900},
   ];
 
   const GoodyBagPaymentData = [
-    { start: "2025-01-01", end: "2025-03-15", amount: 4720 ,bannerAmount:4000},    
-    { start: "2025-03-16", end: "2025-12-31", amount: 7080 , bannerAmount:6000},
+    { start: "2025-01-01", end: "2025-03-19", amount: 4720 ,bannerAmount:4000},    
+    { start: "2025-03-20", end: "2025-12-31", amount: 7080 , bannerAmount:6000},
   ];
 
   const DisplayTablePaymentData = [
-    { start: "2025-01-01", end: "2025-03-15", amount: 4720 ,bannerAmount:4000},  
-    { start: "2025-03-16", end: "2025-03-31", amount: 5900 ,bannerAmount:5000},  
+    { start: "2025-01-01", end: "2025-03-19", amount: 4720 ,bannerAmount:4000},  
+    { start: "2025-03-20", end: "2025-03-31", amount: 5900 ,bannerAmount:5000},  
     { start: "2025-04-01", end: "2025-04-07", amount: 6490 ,bannerAmount:5500},  
     { start: "2025-04-08", end: "2025-12-31", amount: 7000 ,bannerAmount:7000},
   ];
